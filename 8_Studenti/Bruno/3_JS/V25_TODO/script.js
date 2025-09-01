79f0fa2b39ce06d53d11bd25bcc6f4030b828778
bruno9994 <brunooo420@gmail.com>
2025-09-01 10:14:19 +0200

    01.09.

--- 8_Studenti/Bruno/3_JS/V25_TODO/script.js
+++ 8_Studenti/Bruno/3_JS/V25_TODO/script.js
@@ -3,6 +3,12 @@
     var addButton = document.querySelector("#input-add");
     var input = document.querySelector("#input-text");
     var list = document.querySelector("ul");
+    var allButton = document.querySelector("#button-all");
+    var activeButton = document.querySelector("#button-active");
+    var completedButton = document.querySelector("#button-completed");
+    var clearCompletedButton = document.querySelector(
+      "#button-clear-completed"
+    );
 
     function addListItem() {
       var text = input.value;
@@ -11,8 +17,6 @@
         var newItem = createListItem(text);
         list.appendChild(newItem);
         input.value = "";
-
-        console.log(text);
       } else {
         alert("Please enter todo");
       }
@@ -36,6 +40,7 @@
     function addCheckBox(item) {
       var checkBox = document.createElement("input");
       checkBox.setAttribute("type", "checkbox");
+      checkBox.addEventListener("click", checkListItem);
       item.insertBefore(checkBox, item.firstChild);
     }
 
@@ -43,11 +48,86 @@
       var removeButton = document.createElement("div");
       removeButton.innerText = "X";
       removeButton.className = "removeButton";
+      removeButton.addEventListener("click", removeListItem);
       item.appendChild(removeButton);
+    }
+
+    function checkListItem(event) {
+      var checkBox = event.target;
+      if (checkBox.checked) {
+        checkBox.parentNode.style.textDecoration = "line-through";
+      } else {
+        checkBox.parentNode.style.textDecoration = "";
+      }
+    }
+
+    function removeListItem(event) {
+      var removeButton = event.target;
+      removeButton.parentNode.parentNode.remove();
+    }
+
+    function showAll() {
+      var listItems = list.getElementsByTagName("li");
+
+      for (var i = 0; i < listItems.length; i++) {
+        listItems[i].style.display = "";
+      }
+      allButton.disabled = true;
+      activeButton.disabled = false;
+      completedButton.disabled = false;
+    }
+
+    function showActive() {
+      var listItems = list.getElementsByTagName("li");
+
+      for (var i = 0; i < listItems.length; i++) {
+        var check = listItems[i].getElementsByTagName("input");
+        if (check[0].checked) {
+          listItems[i].style.display = "none";
+        } else {
+          listItems[i].style.display = "";
+        }
+      }
+
+      allButton.disabled = false;
+      activeButton.disabled = true;
+      completedButton.disabled = false;
+    }
+
+    function showCompleted() {
+      var listItems = list.getElementsByTagName("li");
+
+      for (var i = 0; i < listItems.length; i++) {
+        var check = listItems[i].getElementsByTagName("input");
+        if (!check[0].checked) {
+          listItems[i].style.display = "none";
+        } else {
+          listItems[i].style.display = "";
+        }
+      }
+
+      allButton.disabled = false;
+      activeButton.disabled = false;
+      completedButton.disabled = true;
+    }
+
+    function removeAllCompleted() {
+      var listItems = list.getElementsByTagName("li");
+
+      for (var i = listItems.length - 1; i >= 0; i--) {
+        var check = listItems[i].getElementsByTagName("input");
+        if (check[0].checked) {
+          listItems[i].remove();
+        }
+      }
     }
 
     this.addListeners = function () {
       addButton.addEventListener("click", addListItem);
+      allButton.addEventListener("click", showAll);
+      activeButton.addEventListener("click", showActive);
+      completedButton.addEventListener("click", showCompleted);
+      clearCompletedButton.addEventListener("click", removeAllCompleted);
     };
   }
 
