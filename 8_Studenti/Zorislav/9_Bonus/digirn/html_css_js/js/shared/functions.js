46bfbcb287cbc90c1ce53f8511aa04e9d70c3ad0
zorislav <137685078+Zori-demo-student@users.noreply.github.com>
2025-09-09 21:08:31 +0200

    Upd 9.9.

--- 8_Studenti/Zorislav/9_Bonus/digirn/html_css_js/js/shared/functions.js
+++ 8_Studenti/Zorislav/9_Bonus/digirn/html_css_js/js/shared/functions.js
@@ -1,5 +1,7 @@
 import { dummyNalozi } from "./data.js";
 
+const API_URL = "https://algebra-fe-default-rtdb.europe-west1.firebasedatabase.app/digirn.json";
+
 export function toggleNav(hamburgerEl, asideEl){
 
   let asideDisplay = window.getComputedStyle(asideEl).display;
@@ -16,13 +18,22 @@ export function toggleNav(hamburgerEl, asideEl){
 
 }
 
-export function ucitajFirebase(){
+export async function ucitajFirebase(){
 
   try {
 
-    // Dihvat podataka
+    // Dohvat podataka
+    const tmpNalozi = [];
 
-    return dummyNalozi;
+    const response = await fetch(API_URL);
+    const data = await response.json();
+
+    for(let key in data){
+      tmpNalozi.push(data[key]);
+    }
+    console.log(tmpNalozi[0]);
+
+    return tmpNalozi[0];
 
   } catch(error) {
     alert(error);
@@ -30,8 +41,38 @@ export function ucitajFirebase(){
 
 }
 
-export function zapisiFirebase(){
-  console.log("Nalozi zapisani!");
+export async function zapisiFirebase(nalozi){
+
+  // Obrisi trenutne naloge
+  await obrisiFirebase();
+
+  // Zapisi naloge
+  try {
+
+    const response = await fetch(API_URL,{
+      method: "POST",
+      body: JSON.stringify(nalozi),
+      headers: {
+        "Content-Type": "application/json"
+      }
+    });
+
+  } catch(error) {
+    alert(error);
+  }
+
 }
 
-function obrisiFirebase(){}
+async function obrisiFirebase(){
+
+    try {
+
+    await fetch(API_URL, {
+      method: "DELETE",
+    });
+
+  } catch(error) {
+    alert(error);
+  }
+
+}
